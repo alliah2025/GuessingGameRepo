817a43181d970de2608a0a165623bb31d4c273b2
Alliah <mahilumalliah002@gmail.com>
2025-04-24 18:59:08 +0800

    Second Commit Bot Feature

--- gclient.py
+++ gclient.py
@@ -1,27 +1,40 @@
-import socket 
+import socket
 
-host = "192.168.88.10"
-# print(host)
-# print(type(host))
+
+host = "localhost" 
 port = 7777
 
 s = socket.socket()
 s.connect((host, port))
 
-data = s.recv(1024)
-print(data.decode().strip())
+data = s.recv(1024).decode()
+print(data)
+difficulty = int(input(""))
+s.sendall(str(difficulty).encode())
+
+if difficulty == 1:
+    low, high = 1, 10
+elif difficulty == 2:
+    low, high = 1, 50
+else:
+    low, high = 1, 100
+
+print(s.recv(1024).decode())
+
 while True:
-    user_input = input("").strip()
-    try:
-        s.sendall(user_input.encode())
-        reply = s.recv(1024).decode().strip()
-        if "CORRECT!" in reply:
-            print(reply)
-            break
-        print(reply)
-        continue
-    except ConnectionAbortedError as e:
-        reply = s.recv(1024).decode().strip()
-        print(reply)
+    guess = (low + high) // 2
+    print(f"BOT: {guess}")
+    s.sendall(str(guess).encode())
+    response = s.recv(1024).decode().strip()
+
+    print(f"SERVER: {response}")
+
+    if "CORRECT!" in response:
         break
+    elif "Lower" in response:
+        high = guess - 1
+    elif "Higher" in response:
+        low = guess + 1
+
+
 s.close()
